9d1e5c0fda4c2cfebeb45f82b9b8f6578cb1f6c2
Beemil <valeskavr17@gmail.com>
2025-11-04 13:48:37 -0600

    modulos integrados

---
+++ 04-modulos.js
@@ -0,0 +1,8 @@
+const os = require ('os');
+
+console.log('Jesus es lo maximo');
+console.log(os.type());
+console.log(os.homedir());
+console.log(os.uptime());
+console.log(os.userInfo());
+console.log(os.hostname());
